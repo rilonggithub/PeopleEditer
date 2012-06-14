de78cafc534a35b390adf415eed28140c73a1562
Rilong <lang19880211@163.com>
2012-06-14 13:41:35 -0700

    Revert "teml"
    This reverts commit 4c8517eb97324e4c0ac1ef78f1f539a5dd64e755.

---
+++ PeopleEditerJQuery/JQueryMVCAjax/ADHelper.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
+
+
+namespace LMXCommonTool
+{
+    public class ADHelper
+    {
+        private static string[] strDomains = new string[] { "Redmond", "Africa", "Europe", "FarEast", "MiddleEast", "NorthAmerica", "SouthAmerica", "SouthPacific", "Sys-WinGroup" };
+        private enum SearchType
+        {
+            byAccount,//like v-lianl;
+            byDisplayname//like Liang Liu(Beijing Aotesiqi);
+        }
+
+        private static List<string> SpiteUsersList(string inputUserList)
+        {
+            List<string> retList = new List<string>();
+            string[] Users = inputUserList.Split(new char[] { ';' });
+            foreach (string item in Users)
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    retList.Add(item);
+                }
+            }
+            return retList;
+        }
+
+        public static List<UserInfo> RetriveUsersListFromAD(string stringInputUsers,out StringBuilder errorAlias)
+        {
+            errorAlias = new StringBuilder ();
+            List<UserInfo> retList = new List<UserInfo>();
+            List<string> users = SpiteUsersList(stringInputUsers);
+            foreach (string item in users)
+            {
+                try
+                {
+                    retList.Add(RetriveUserFromAD(item));
+                }
+                catch { errorAlias.Append(item+";"); }
+            }
+            return retList;
+        }
+        public static string RetriveUsersFromAD(string stringInputUsers)
+        {
+            StringBuilder success = new StringBuilder();
+            StringBuilder failed = new StringBuilder();
+            UserInfo ui;
+
+            List<string> users = SpiteUsersList(stringInputUsers);
+            foreach (string item in users)
+            {
+                if (!string.IsNullOrEmpty(item.Trim()))
+                {
+                    ui = RetriveUserFromAD(item);
+
+                    if (ui.IsInAD)
+                    {
+                        if (!success.ToString().Contains(ui.userDisplayName))
+                            success.Append(ui.userDisplayName + ";");
+                    }
+                    else
+                    {
+                        if (!failed.ToString().Contains(ui.queryName))
+                        {
+                            failed.Append(ui.queryName + ";");
+                        }
+                    }
+                }
+            }
+
+            return success.ToString() + "ADErrors at : " + failed.ToString();
+        }
+
+        public static string RetriveUsersFromADSuccess(string stringInputUsers)
+        {
+            StringBuilder success = new StringBuilder();
+            StringBuilder failed = new StringBuilder();
+            UserInfo ui;
+
+            List<string> users = SpiteUsersList(stringInputUsers);
+            foreach (string item in users)
+            {
+                if (!string.IsNullOrEmpty(item.Trim()))
+                {
+                    ui = RetriveUserFromAD(item);
+
+                    if (ui.IsInAD)
+                    {
+                        if (!success.ToString().Contains(ui.userDisplayName))
+                            success.Append(ui.userDisplayName + ";");
+                    }
+                    else
+                    {
+                        if (!failed.ToString().Contains(ui.queryName))
+                        {
+                            failed.Append(ui.queryName + ";");
+                        }
+                    }
+                }
+            }
+
+            return success.ToString() + failed.ToString();
+        }
+
+        public static List<string> RetriveUserMailsFromAD(string stringInputUsers)
+        {
+            StringBuilder retString = new StringBuilder();
+            UserInfo ui;
+
+            List<string> users = SpiteUsersList(stringInputUsers);
+            List<string> mailsList = new List<string>();
+            foreach (string item in users)
+            {
+                ui = RetriveUserFromAD(item);
+                if (ui.IsInAD)
+                {
+                    mailsList.Add(ui.userEmail);
+                }
+            }
+
+            return mailsList;
+        }
+
+        public static List<string> RetriveUserMailsFromAD(List<string> users)
+        {
+            StringBuilder retString = new StringBuilder();
+            UserInfo ui;
+
+            List<string> mailsList = new List<string>();
+            foreach (string item in users)
+            {
+                ui = RetriveUserFromAD(item);
+                if (ui.IsInAD)
+                {
+                    mailsList.Add(ui.userEmail);
+                }
+            }
+
+            return mailsList;
+        }
+
+        private static UserInfo RetriveUserFromAD(string inputUser)
+        {
+            if (GetSearchType(inputUser) == SearchType.byAccount)
+            {
+                return CheckNameByAccount(inputUser);
+            }
+            else
+                return CheckNameByDisplayName(inputUser);
+        }
+
+        private static SearchType GetSearchType(string strName)
+        {
+            if (strName.IndexOf("\\") > -1)
+                return SearchType.byAccount;
+            else
+                return SearchType.byDisplayname;
+        }
+
+        private static UserInfo CheckNameByAccount(string userName)
+        {
+            UserInfo ui = new UserInfo();
+            try
+            {
+                string domain = GetDomain(userName);
+                string loginName = GetLoginName(userName);
+                string domainPath = GetDomainPath(domain);
+                DirectoryEntry de = new DirectoryEntry(domainPath);
+                DirectorySearcher deSearch = new DirectorySearcher();
+                deSearch.SearchRoot = de;
+                deSearch.Filter = "(&(objectClass=user)(objectCategory=person)(samaccountname=" + loginName + "))";
+                SearchResult results = deSearch.FindOne();
+                if (results != null)
+                {
+                    ui.userAlias = domain + "\\" + results.Properties["samaccountname"][0].ToString();
+                    ui.userDisplayName = results.Properties["displayname"][0].ToString();
+                    ui.userEmail = results.Properties["mail"][0].ToString();
+                    ui.queryName = userName;
+                    ui.IsInAD = true;
+                }
+            }
+            catch (Exception)
+            {
+                //Catch Incorrect Domain name
+            }            
+            ui.queryName = userName;
+            return ui;
+        }
+
+        private static string GetDomain(string userName)
+        {
+            string strDomain = string.Empty;
+            strDomain = userName.Split('\\')[0];
+            DirectoryContext objContext = new DirectoryContext(DirectoryContextType.Domain, strDomain, @"Redmond\lmxsys", "0p;/)P:?");
+            string domain = Domain.GetDomain(objContext).Name;
+            return domain;
+        }
+
+        private static string GetLoginName(string userName)
+        {
+            string strLoginName = string.Empty;
+            strLoginName = userName.Split('\\')[1];
+            return strLoginName;
+        }
+
+        private static string GetDomainPath(string domain)
+        {
+            return "LDAP://" + domain;
+        }
+        private static UserInfo CheckNameByDisplayName(string userName)
+        {
+            userName = userName.Trim();
+            UserInfo ui = new UserInfo();
+            string domainPath = string.Empty;
+
+            foreach (string domain in strDomains)
+            {
+                domainPath = GetDomainPath(domain);
+                DirectoryEntry de = new DirectoryEntry(domainPath, "lmxsys", "qwe123!@#");
+                DirectorySearcher deSearch = new DirectorySearcher();
+                deSearch.SearchRoot = de;
+                deSearch.Filter = "(&(objectClass=user)(objectCategory=person)(samaccountname=" + userName + "))";
+                SearchResult results = deSearch.FindOne();
+
+                if (results != null)
+                {
+                    ui.userAlias = domain + "\\" +   results.Properties["samaccountname"][0].ToString();
+                    ui.userDisplayName = results.Properties["displayname"][0].ToString();
+                    ui.userEmail = results.Properties["mail"][0].ToString();
+                    ui.queryName = userName;
+                    ui.IsInAD = true;
+                    break;
+                }
+                else
+                {
+                    ui.queryName = userName;
+                }
+            }
+            return ui;
+        }
+
+        public static bool CheckUserInGroup(string strUserName,string strGroupName)
+        {
+            bool check = false;
+           
+            foreach (string domain in strDomains)
+            {
+               // domainPath = GetDomainPath(domain);
+                try
+                {
+                    using (var pc = new PrincipalContext(ContextType.Domain, domain))
+                    using (var user = UserPrincipal.FindByIdentity(pc, IdentityType.SamAccountName, strUserName))
+                    // using (var group = GroupPrincipal.FindByIdentity(pc, "MOD PMG Updates"))
+                    using (var group = GroupPrincipal.FindByIdentity(pc, strGroupName))
+                    {
+                        check = user.IsMemberOf(group);
+                        if (check)
+                            break;
+                    }
+                }
+                catch (Exception e)
+                { 
+                  
+                }
+            }
+            return check;
+          
+        }
+    }
+}
---
+++ PeopleEditerJQuery/JQueryMVCAjax/Controllers/CheckAliasController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.Mvc;
+using LMXCommonTool;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace JQueryMVCAjax.Controllers
+{
+    public class CheckAliasController : Controller
+    {
+        //
+        // GET: /CheckAlias/
+        
+        private StringBuilder errorAlias =new StringBuilder ();
+        private List<UserInfo> SampleData = new List<UserInfo> { 
+            new UserInfo{ queryName="v-huicai", userDisplayName="Hui Cai (Beijing Aotesiqi)", userAlias=@"redmond\v-huicai"},
+            new UserInfo{ queryName="v-zixyin", userDisplayName="ZiXin Yin (Beijing Aotesiqi)", userAlias=@"redmond\v-zixyin"},
+            new UserInfo{ queryName="v-lefeil", userDisplayName="LeFei li (Beijing Aotesiqi)", userAlias=@"redmond\v-lefeil"},
+            new UserInfo{ queryName="v-bopeng", userDisplayName="Bo Peng (Beijing Aotesiqi)", userAlias=@"redmond\v-bopeng"},
+            new UserInfo{ queryName="lmxsys", userDisplayName="LMX Syetem", userAlias=@"redmond\LMXSYS"}
+        };
+        List<UserInfo> listUserInfo = new List<UserInfo>();
+        private ResolveResult result = new ResolveResult();
+        private string[] aliasArray;
+        public JsonResult Index(string aliasList)
+        {
+            string output;
+            try
+            {
+                GetUserFromSampleData(aliasList);
+                output = JsonConvert.SerializeObject(result);
+            }
+            catch (Exception e)
+            {
+                output = e.StackTrace;
+            }
+
+            for (int i = 0; i < 900000000; i++) ; for (int i = 0; i < 900000000; i++) ;
+            for (int i = 0; i < 900000000; i++) ; for (int i = 0; i < 900000000; i++) ;
+            for (int i = 0; i < 900000000; i++) ; for (int i = 0; i < 900000000; i++) ;
+
+                return Json(output, JsonRequestBehavior.AllowGet);
+        }
+
+
+        /*
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+        private void GetUserFromAD(string aliasList)
+        {
+            listUserInfo = ADHelper.RetriveUsersListFromAD(aliasList, out errorAlias);
+            GetJson();
+        }
+
+        private void GetUserFromSampleData(string aliasList)
+        {
+            
+            bool IsResolve = false;
+            aliasArray = aliasList.Split(';');
+            foreach (string alias in aliasArray)
+            {
+                foreach (UserInfo user in SampleData)
+                {
+                    if (alias.ToLower().Trim().Equals(user.queryName))
+                    {
+                        listUserInfo.Add(user);
+                        IsResolve = true;
+                    }
+                }
+                if (IsResolve == false)
+                {
+                    errorAlias.Append(alias + ";");
+                    IsResolve = false;
+                }
+                IsResolve = false;
+
+            }
+            GetJson();
+        }
+
+        private void GetJson()
+        {
+
+
+            int index = 0;
+            Resolved[] resolved = new Resolved[listUserInfo.Count];
+            foreach (UserInfo user in listUserInfo)
+            {
+                Resolved re = new Resolved();
+                re.AccountName = user.userAlias;
+                re.DisplayName = user.userDisplayName;
+                re.OriginalText = user.queryName;
+                resolved[index++] = re;
+            }
+            result.ResolvedResult = resolved;
+            if (errorAlias.ToString().Length >  0 ){
+                ResolveError error=new ResolveError ();
+                error.OriginalText = errorAlias.ToString();
+                result.ResolvedErrorResult=error;
+            }
+        }
+
+    }
+}
